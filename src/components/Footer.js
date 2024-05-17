0f398bea2252e02d644cd4d94d4c40e2e7917c18
SRanuluge <sranuluge.dev@gmail.com>
2024-05-17 15:14:15 +1200

    added meta data

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer>
+      <img/>
+      <ul>
+        <li>
+          <a href="">Contact info</a>
+        </li>
+        <li>
+          <a href="">Links to social-media</a>
+        </li>
+        <li>
+          <a href="">Doormat Navigation</a>
+        </li>
+        
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
